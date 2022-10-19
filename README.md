b318696912fcade687c034dd7fe18dd1248f2f51
Riley Rahe <rilrahe@gmail.com>
2022-10-18 19:36:34 -0500

    final final touches

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Daily-Planner
-Challenge for week 5 
+Challenge for week 5. Scope of the project was to make daily planner that changed as the day went on. Past in grey, present in red, future in green. 
+
+
+[Example](./Assets/Webpage.png)
--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 //get the info stored if any
 $("#hour8 .task-info").val(localStorage.getItem("hour8"));
+$("#hour8.5 .task-info").val(localStorage.getItem("hour8.5"));
 $("#hour9 .task-info").val(localStorage.getItem("hour9"));
+$("#hour9.5 .task-info").val(localStorage.getItem("hour9.5"));
 $("#hour10 .task-info").val(localStorage.getItem("hour10"));
+$("#hour10.5 .task-info").val(localStorage.getItem("hour10.5"));
 $("#hour11 .task-info").val(localStorage.getItem("hour11"));
+$("#hour11.5 .task-info").val(localStorage.getItem("hour11.5"));
 $("#hour12 .task-info").val(localStorage.getItem("hour12"));
+$("#hour12.5 .task-info").val(localStorage.getItem("hour12.5"));
 $("#hour13 .task-info").val(localStorage.getItem("hour13"));
+$("#hour13.5 .task-info").val(localStorage.getItem("hour13.5"));
 $("#hour14 .task-info").val(localStorage.getItem("hour14"));
+$("#hour14.5 .task-info").val(localStorage.getItem("hour14.5"));
 $("#hour15 .task-info").val(localStorage.getItem("hour15"));
+$("#hour15.5 .task-info").val(localStorage.getItem("hour15.5"));
 $("#hour16 .task-info").val(localStorage.getItem("hour16"));
+$("#hour16.5 .task-info").val(localStorage.getItem("hour16.5"));
 $("#hour17 .task-info").val(localStorage.getItem("hour17"));
+$("#hour17.5 .task-info").val(localStorage.getItem("hour17.5"));
 
 // makes a little clock that shows current time, updates every second
 var timeDisplayEl = $("#time-display");
@@ -35,23 +45,9 @@ $(document).ready(function () {
     //get current number of hours.
     var currentTime = moment().hour();
 
-    // loop over time blocks
+    // loop over time blocks to check against current time for class
     $(".time-block").each(function () {
       var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-      // var blockTime = [
-      //   $("hour8"),
-      //   $("hour9"),
-      //   $("hour10"),
-      //   $("hour11"),
-      //   $("hour12"),
-      //   $("hour13"),
-      //   $("hour14"),
-      //   $("hour15"),
-      //   $("hour16"),
-      //   $("hour17")
-      // ];
-      // blockTime.split('hour')
-      // To check time block against current time for color
       if (blockTime === currentTime) {
         $(this).removeClass("past");
         $(this).removeClass("future");
@@ -60,7 +56,7 @@ $(document).ready(function () {
         $(this).removeClass("future");
         $(this).removeClass("present");
         $(this).addClass("past");
-      } else if (blockTime>currentTime) {
+      } else if (blockTime > currentTime) {
         $(this).removeClass("present");
         $(this).removeClass("past");
         $(this).addClass("future");
@@ -71,7 +67,8 @@ $(document).ready(function () {
   checkTime();
 });
 
-//Liked this as an idea but having trouble making it do the thing 
-$(".clearBtn").on("click", function () {
-  localStorage.clear(time, text);
-});
+//Liked this as an idea but having trouble making it do the thing, if working it clears local storage on button press. in theory
+function clearBtn() {
+  localStorage.clear();
+}
+$(".clearBtn").on("click", clearBtn());
